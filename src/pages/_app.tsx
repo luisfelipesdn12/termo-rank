53b6b01f15b6523aebf2b8a7242a16d582fca960
Luis Felipe <luisfelipesdn12@gmail.com>
2022-01-23 00:10:30 -0300

    feat: Add metatags

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,6 +11,37 @@ export default function TermoRank({ Component, pageProps }: AppProps) {
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
                 <link href="https://fonts.googleapis.com/css2?family=Mitr:wght@300;400;500;600;700&amp;display=swap" rel="stylesheet" />
                 <link rel="shortcut icon" href="/icon.png" />
+
+                {/* <!-- Primary Meta Tags --> */}
+                <title>Termo Rank</title>
+                <meta name="title" content="Termo Rank" />
+                <meta name="description" content="Este é um ranking para o jogo de advinhação de palavras Termo e foi desenvolvido por Luis Felipe.
+
+Termo foi inspirado no Wordle desenvolvido por Fernando Serboncini.
+
+A partir do código que foi fornecido, você tem acesso à sua conta para adicionar suas estatísticas da palavra de hoje."/>
+
+                {/* <!-- Open Graph / Facebook --> */}
+                <meta property="og:type" content="website" />
+                <meta property="og:url" content="https://termo-rank.vercel.app/" />
+                <meta property="og:title" content="Termo Rank" />
+                <meta property="og:description" content="Este é um ranking para o jogo de advinhação de palavras Termo e foi desenvolvido por Luis Felipe.
+
+Termo foi inspirado no Wordle desenvolvido por Fernando Serboncini.
+
+A partir do código que foi fornecido, você tem acesso à sua conta para adicionar suas estatísticas da palavra de hoje."/>
+                <meta property="og:image" content="" />
+
+                {/* <!-- Twitter --> */}
+                <meta property="twitter:card" content="summary_large_image" />
+                <meta property="twitter:url" content="https://termo-rank.vercel.app/" />
+                <meta property="twitter:title" content="Termo Rank" />
+                <meta property="twitter:description" content="Este é um ranking para o jogo de advinhação de palavras Termo e foi desenvolvido por Luis Felipe.
+
+Termo foi inspirado no Wordle desenvolvido por Fernando Serboncini.
+
+A partir do código que foi fornecido, você tem acesso à sua conta para adicionar suas estatísticas da palavra de hoje."/>
+                <meta property="twitter:image" content="" />
             </Head>
             <Component {...pageProps} />
         </>
